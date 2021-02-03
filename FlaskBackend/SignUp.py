55c1881b9a8372869ac7ed0a420d7f0df0388ee7
VenuraRajapaksha-2000 <60282548+VenuraRajapaksha-2000@users.noreply.github.com>
2021-02-03 18:04:45 +0530

    venura

--- FlaskBackend/SignUp.py
+++ FlaskBackend/SignUp.py
@@ -1,7 +1,16 @@
 class SignUp:
 
-    def __init__(self, database):
+
+    def __init__(self , database , userName , email , password ):
         self.database = database
+
 
     def signUpAccount(self):
         return
+
+
+
+
+
+
+
