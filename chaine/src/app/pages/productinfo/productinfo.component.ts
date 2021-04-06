c51eb9b75cd3376211d85f7ffd43092af8622c11
seneshsirimanne00 <seneshsirimanne@gmail.com>
2021-04-06 14:10:58 +0530

    csv reader moved to product info page

--- chaine/src/app/pages/productinfo/productinfo.component.ts
+++ chaine/src/app/pages/productinfo/productinfo.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DatatransferService } from 'src/app/datatransfer.service';
 import { ProductInfoData } from 'src/app/data_interfaces/ProductInfoData';
-
+import {ViewChild} from '@angular/core';
 @Component({
   selector: 'app-productinfo',
   templateUrl: './productinfo.component.html',
@@ -15,6 +15,9 @@ export class ProductinfoComponent implements OnInit {
   prodtime:string;
   rawmaterialqty : string;
 
+  public csvRecords: any[] = [];
+
+  @ViewChild('fileImportInput') fileImportInput: any;
   constructor(private datatransfer : DatatransferService) { }
 
   listOfDataPInfo : ProductInfoData[];// for table
@@ -39,4 +42,87 @@ export class ProductinfoComponent implements OnInit {
 
   }
 
+  fileChangeListener($event: any): void {
+
+    let text = [];
+    let files = $event.srcElement.files;
+
+    if (this.isCSVFile(files[0])) {
+
+      let input = $event.target;
+      let reader = new FileReader();
+      reader.readAsText(input.files[0]);
+
+      reader.onload = () => {
+        let csvData = reader.result;
+        let csvRecordsArray = (<string>csvData).split(/\r\n|\n/);
+
+        let headersRow = this.getHeaderArray(csvRecordsArray);
+
+        this.csvRecords = this.getDataRecordsArrayFromCSVFile(csvRecordsArray, headersRow.length);
+        
+        
+        this.datatransfer.sendCSVData(this.csvRecords).subscribe();
+      };
+
+      reader.onerror = function () {
+        alert('Unable to read ' + input.files[0]);
+      };
+
+    } else {
+      alert("Please import valid .csv file.");
+      this.fileReset();
+    }
+  }
+
+  getDataRecordsArrayFromCSVFile(csvRecordsArray: any, headerLength: any) {
+    let dataArr = [];
+
+    for (let i = 1; i < csvRecordsArray.length; i++) {
+      let data = (<string>csvRecordsArray[i]).split(',');
+
+      // FOR EACH ROW IN CSV FILE IF THE NUMBER OF COLUMNS
+      // ARE SAME AS NUMBER OF HEADER COLUMNS THEN PARSE THE DATA
+      if (data.length == headerLength) {
+
+        let csvRecord: CSVRecord = new CSVRecord();
+
+        csvRecord.date = data[0].trim();
+        csvRecord.store = data[1].trim();
+        csvRecord.item = data[2].trim();
+        csvRecord.sales = data[3].trim();
+
+        dataArr.push(csvRecord);
+      }
+    }
+    return dataArr;
+  }
+
+  // CHECK IF FILE IS A VALID CSV FILE
+  isCSVFile(file: any) {
+    return file.name.endsWith(".csv");
+  }
+
+  // GET CSV FILE HEADER COLUMNS
+  getHeaderArray(csvRecordsArr: any) {
+    let headers = (<string>csvRecordsArr[0]).split(',');
+    let headerArray = [];
+    for (let j = 0; j < headers.length; j++) {
+      headerArray.push(headers[j]);
+    }
+    return headerArray;
+  }
+
+  fileReset() {
+    this.fileImportInput.nativeElement.value = "";
+    this.csvRecords = [];
+  }
+
+}
+
+export class CSVRecord {
+  public date: any;
+  public store: any;
+  public item: any;
+  public sales: any;
 }
